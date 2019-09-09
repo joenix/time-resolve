f32e9df24244e14771f0b39873e86368eb750bb4
Joenix <joenix@qq.com>
2019-09-09 22:20:42 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = ( time, fmt ) => {
+
+  const rule = {
+    'M+': time.getMonth() + 1,
+    'd+': time.getDate(),
+    'h+': time.getHours(),
+    'm+': time.getMinutes(),
+    's+': time.getSeconds(),
+    'q+': Math.floor((time.getMonth() + 3) / 3),
+    'S':  time.getMilliseconds()
+  }
+
+  if (/(y+)/.test(fmt))
+  {
+    fmt = fmt.replace(RegExp.$1, (time.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+
+  for (let k in rule)
+  {
+    if (new RegExp('(' + k + ')').test(fmt))
+    {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (rule[k]) : (('00' + rule[k]).substr(('' + rule[k]).length)))
+    }
+  }
+
+  return fmt
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Time Resolve
+
+```javascript
+import timeResolve from 'time-resolve'
+
+timeResolve(new Date(), 'yyyy-MM-dd hh:mm:ss') // 1963-10-10 00:20:40
+```
